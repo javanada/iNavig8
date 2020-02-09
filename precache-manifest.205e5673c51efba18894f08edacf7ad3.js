1f15be7970d69f81f5e505d22ace1dc8e4665472
jcampbell18 <jcampbell18@eagles.ewu.edu>
2020-02-09 12:22:29 -0800

    Updates

---
+++ precache-manifest.205e5673c51efba18894f08edacf7ad3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40fd5484263c8d6fe775bf690a8d30e",
+    "url": "/iNavig8/index.html"
+  },
+  {
+    "revision": "e53f6a7bff162f41ace0",
+    "url": "/iNavig8/static/css/2.1c29a7ee.chunk.css"
+  },
+  {
+    "revision": "0d945b514b93fd54836b",
+    "url": "/iNavig8/static/css/main.b3d41517.chunk.css"
+  },
+  {
+    "revision": "e53f6a7bff162f41ace0",
+    "url": "/iNavig8/static/js/2.6cef236e.chunk.js"
+  },
+  {
+    "revision": "0d945b514b93fd54836b",
+    "url": "/iNavig8/static/js/main.1004a765.chunk.js"
+  },
+  {
+    "revision": "839303acfc11d3fe2b27",
+    "url": "/iNavig8/static/js/runtime-main.2df75afb.js"
+  },
+  {
+    "revision": "0698c4ef2c4d0092eb1f3686c17da734",
+    "url": "/iNavig8/static/media/background.0698c4ef.jpg"
+  }
+]);
